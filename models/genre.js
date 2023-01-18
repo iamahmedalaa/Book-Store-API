95b093a9735d9c78b13b8c0533190c1c59231db2
Ahmed Alaa <ahmed.alaa50.aa@gmail.com>
2023-01-18 20:35:05 +0200

    Add files via upload

---
+++ models/genre.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+// Genre Schema
+const genreSchema = mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	},
+	create_date:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+const Genre = module.exports = mongoose.model('Genre', genreSchema);
+
+// Get Genres
+module.exports.getGenres = (callback, limit) => {
+	Genre.find(callback).limit(limit);
+}
+
+// Add Genre
+module.exports.addGenre = (genre, callback) => {
+	Genre.create(genre, callback);
+}
+
+// Update Genre
+module.exports.updateGenre = (id, genre, options, callback) => {
+	 let query = {_id: id};
+	 let update = {
+		name: genre.name
+	}
+	Genre.findOneAndUpdate(query, update, options, callback);
+}
+
+
+// Delete Genre
+module.exports.removeGenre = (id, callback) => {
+	 let query = {_id: id};
+	Genre.deleteOne(query, callback);
+}
